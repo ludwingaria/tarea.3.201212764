469751c1bb4487bb31117690c11189520f883028
Majosh <Majosh@192.168.1.6>
2017-08-09 20:14:43 -0600

    tarea3_201212764 commit

---
+++ src/tarea3/Tarea3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Majosh
+ */
+public class Tarea3 { 
+
+    
+
+
+public static void main(String args[]) throws IOException  { 
+   MenuPrincipal2 tarea3=new MenuPrincipal2();
+   
+   tarea3.menu();
+}
+}
+    
+    
